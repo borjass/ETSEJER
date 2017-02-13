85d6c1620eb79a68029a1839fb4312ad76056307
borjass <borjasantanasuarez@hotmail.com>
2017-02-13 20:31:08 +0000

    Add files via upload

---
+++ SMI.java
@@ -0,0 +1,14 @@
+package PriEva;
+import java.util.Scanner;
+public class SMI {
+	public static void main(String[] args) {
+		//Comentario comentado
+		double longitud;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Radio: ");
+		double radio=sc.nextDouble();
+		longitud = 2 * 3.1416 + radio;
+		System.out.println("Longitud: "+longitud);
+		sc.close();
+	}
+}
